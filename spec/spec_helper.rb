25efee83a633480d82fb01f07129a33136c7ff3f
github username <github email address>
2017-10-10 13:51:23 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,15 @@
-ENV["SINATRA_ENV"] = "test"
-require_relative '../config/environment'
-require 'rack/test'
-require 'capybara/poltergeist'
-
-Capybara.javascript_driver = :poltergeist
-
-RSpec.configure do |config|
-  config.include Capybara::DSL
-  config.include Rack::Test::Methods
-  config.order = 'default'
-end
-
-def app
-  Rack::Builder.parse_file('config.ru').first
-end
-
-Capybara.app = app
+# ENV["SINATRA_ENV"] = "test"
+# require_relative '../config/environment'
+# require 'rack/test'
+#
+# RSpec.configure do |config|
+#   config.include Capybara::DSL
+#   config.include Rack::Test::Methods
+#   config.order = 'default'
+# end
+#
+# def app
+#   Rack::Builder.parse_file('config.ru').first
+# end
+#
+# Capybara.app = app
